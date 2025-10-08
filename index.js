d0b696d76f2faddc98857de8d42a82eb6a8c8a79
jasonz0704 <oonetwome@gmail.com>
2025-10-08 11:00:44 -0700

    2

---
+++ index.js
@@ -0,0 +1,108 @@
+const Below 500k= [
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+]
+const 500k - 1M= [
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+]
+const 1M - 1.5M= [
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+]
+const Above 1.5M= [
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+    {
+        name: "Blue Lamborghini",
+        price: 450000,
+        image: "https://example.com/blue-lamborghini.jpg"
+    }
+]
